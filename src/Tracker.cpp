6ff0c4c0da0b985afc14e31cdc91072d31f41dba
KyleLEI <leizhao2@gmail.com>
2019-04-13 17:46:55 +0800

    converted Tracker to GPU

--- src/Tracker.cpp
+++ src/Tracker.cpp
@@ -11,22 +11,27 @@ void Tracker::track(const GpuMat im_prev, const GpuMat im_gray, const vector<Poi
     if (points_prev.size() > 0)
     {
         vector<float> err; //Needs to be float
+
         /* Convert everything to GpuMat */
         GpuMat points_prev_gpu(points_prev),points_tracked_gpu(points_tracked),status(status_gpu),err(err_gpu);
         //Calculate forward optical flow for prev_location
         cuda_of->calc(im_prev, im_gray, points_prev_gpu, points_tracked_gpu, status_gpu, err_gpu);
         
-        /* Download useful stuff back to CPU */
-        
-
         vector<Point2f> points_back;
         vector<unsigned char> status_back;
         vector<float> err_back; //Needs to be float
-
+		
+		/* Convert everything to GpuMat */
+		GpuMat points_back_gpu(points_back),status_back_gpu(status_back),err_back_gpu(err_back);
         //Calculate backward optical flow for prev_location
-        cuda_of->calc(im_gray, im_prev, points_tracked, points_back, status_back, err_back);
+        cuda_of->calc(im_gray, im_prev, points_tracked_gpu, points_back_gpu, status_back_gpu, err_back_gpu);
 
-        // TODO: download useful stuff back
+		/* Download points_tracked back to CPU */
+		points_tracked_gpu.download(points_tracked);
+		points_back_gpu.download(points_back);
+		status_gpu.download(status);
+		status_back_gpu.download(status_back);
+		
 
         //Traverse vector backward so we can remove points on the fly
         for (int i = points_prev.size()-1; i >= 0; i--)
